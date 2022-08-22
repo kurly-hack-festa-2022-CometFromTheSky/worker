186c40a48b3488cb4ef46233b6ef1dea8562ff7c
AllyHyeseongKim <ally3k@icloud.com>
2022-08-22 20:43:45 +0900

    [feature/worker] Create the service
    src/main/java/io/cometkey/worker/service/WorkerService.java

--- src/main/java/io/cometkey/worker/service/WorkerService.java
+++ src/main/java/io/cometkey/worker/service/WorkerService.java
@@ -1,4 +1,33 @@
 package io.cometkey.worker.service;
 
+import io.cometkey.worker.domain.Key;
+import io.cometkey.worker.repository.KeyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class WorkerService {
+
+    private final KeyRepository keyRepository;
+
+    public List<Key> getKey(List<Long> idList) { return this.keyRepository.findAllById(idList); }
+    public String getEncryptedKey(Long keyId) {
+        Key key = this.keyRepository.findById(keyId).get();
+        if (key.getIsUsed()) {
+            return "[Used Key Error] Key is expired.";
+        } else {
+            return key.getEncryptedKey();
+        }
+    }
+
+    public Long addNewKey(Key key) { return this.keyRepository.save(key).getId(); }
+    public void expireUsedKey(Long keyId) {
+        Key key = this.keyRepository.findById(keyId).get();
+        key.setIsUsed(true);
+        this.keyRepository.save(key);
+    }
+
 }
